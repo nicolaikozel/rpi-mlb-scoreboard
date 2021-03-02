ac77f54ffa26e7ad8ffe5c68d496b8c9e50385e8
Nicolai Kozel <nicolaikozel@live.ca>
2021-03-02 17:31:02 -0500

    Render an image

--- views/weather.py
+++ views/weather.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from PIL import Image
 
 from rgbmatrix import FrameCanvas, graphics, RGBMatrix
 
 from constants import Color, Font
 from data import Data
 from graphics.text import center_text_position
+from utils import get_abs_file_path
 from views.base_views import BaseView
 from weather.open_weather import OpenWeatherDataThread
 
@@ -22,6 +24,7 @@ class WeatherView(BaseView):
         self._temperature_color = temperature_color
         self._day_of_the_week_color = day_of_the_week_color
         self._offscreen_canvas = self._rgb_matrix.CreateFrameCanvas()
+        self._icon = None
 
     def _render_temperature(self):
         weather_data = Data.get("weather")
@@ -49,10 +52,21 @@ class WeatherView(BaseView):
             self._day_of_the_week_color.value,
             day_of_the_week,
         )
+    
+    def _render_icon(self):
+        if not self._icon:
+            self._icon = Image.open(get_abs_file_path("assets/partially_cloudy.ppm")).convert('RGB')
+        self._icon.resize((self._rgb_matrix.width, self._rgb_matrix.height), Image.ANTIALIAS)
+
+        icon_width, _ = self._icon.size
+        xpos = 1
+        self._offscreen_canvas.SetImage(self._icon, unsafe=False)
+        self._offscreen_canvas.SetImage(self._icon, unsafe=False)
 
     def _render(self):
         self._offscreen_canvas.Clear()
 
+        self._render_icon()
         self._render_temperature()
         self._render_day_of_the_week()
 
