84634f7ad8b6b2a50a6aaaeba85f00f600b7eed8
Nicolai Kozel <nicolaikozel@live.ca>
2021-03-06 15:52:45 -0500

    Black

--- graphics/common.py
+++ graphics/common.py
@@ -2,5 +2,7 @@ def get_x_pos_for_centered_text(center_pos: int, text: str, font_width: int) -> 
     return abs(center_pos - ((len(text) * font_width) / 2))
 
 
-def get_x_pos_for_centered_text_and_obj(center_pos: int, text: str, font_width: int, obj_width: int) -> int:
+def get_x_pos_for_centered_text_and_obj(
+    center_pos: int, text: str, font_width: int, obj_width: int
+) -> int:
     return abs(center_pos - ((obj_width + (len(text) * font_width)) / 2))
--- views/weather.py
+++ views/weather.py
@@ -52,7 +52,9 @@ class WeatherView(BaseView):
         self._condition = None
         self._temperature_color = temperature_color
 
-    def _render_temperature(self, temperature: str, font: Font, font_size: Dict, x_pos: int):
+    def _render_temperature(
+        self, temperature: str, font: Font, font_size: Dict, x_pos: int
+    ):
         graphics.DrawText(
             self._offscreen_canvas,
             font,
@@ -61,17 +63,29 @@ class WeatherView(BaseView):
             self._temperature_color.value,
             temperature,
         )
-        graphics.DrawCircle(self._offscreen_canvas, x_pos+len(temperature)*font_size["width"], 4, 1, self._temperature_color.value)
-    
+        graphics.DrawCircle(
+            self._offscreen_canvas,
+            x_pos + len(temperature) * font_size["width"],
+            4,
+            1,
+            self._temperature_color.value,
+        )
+
     def _render_condition(self, condition: WeatherCondition, x_pos: int):
         if condition != self._condition:
             icon_data = WEATHER_CONDITION_ICON_MAP[condition]
-            self._icon = Image.open(get_abs_file_path(f"assets/{icon_data['name']}.ppm")).convert('RGB')
+            self._icon = Image.open(
+                get_abs_file_path(f"assets/{icon_data['name']}.ppm")
+            ).convert("RGB")
             self._icon_x_offset = icon_data.get("x_offset", 0)
             self._icon_y_offset = icon_data.get("y_offset", 0)
             self._condition = condition
-        self._icon.resize((self._rgb_matrix.width, self._rgb_matrix.height), Image.ANTIALIAS)
-        self._offscreen_canvas.SetImage(self._icon, x_pos+self._icon_x_offset, self._icon_y_offset, unsafe=False)
+        self._icon.resize(
+            (self._rgb_matrix.width, self._rgb_matrix.height), Image.ANTIALIAS
+        )
+        self._offscreen_canvas.SetImage(
+            self._icon, x_pos + self._icon_x_offset, self._icon_y_offset, unsafe=False
+        )
 
     def _render(self):
         self._offscreen_canvas.Clear()
@@ -80,11 +94,24 @@ class WeatherView(BaseView):
         temperature = "-10"
         condition = WeatherCondition.CLOUDS
         if weather_data:
-            #temperature = str(weather_data.current.temperature)
+            # temperature = str(weather_data.current.temperature)
             condition = weather_data.current.condition
         font, size = self._get_font(Font.TINY)
-        x_pos = get_x_pos_for_centered_text_and_obj(center_pos=16, text=temperature, font_width=size["width"], obj_width=ICON_WIDTH) - 1
+        x_pos = (
+            get_x_pos_for_centered_text_and_obj(
+                center_pos=16,
+                text=temperature,
+                font_width=size["width"],
+                obj_width=ICON_WIDTH,
+            )
+            - 1
+        )
         self._render_condition(condition=condition, x_pos=x_pos)
-        self._render_temperature(temperature=temperature, font=font, font_size=size, x_pos=x_pos+ICON_WIDTH-1)
+        self._render_temperature(
+            temperature=temperature,
+            font=font,
+            font_size=size,
+            x_pos=x_pos + ICON_WIDTH - 1,
+        )
 
         self._offscreen_canvas = self._rgb_matrix.SwapOnVSync(self._offscreen_canvas)
