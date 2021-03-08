b5cb2292a7efd6bfe41464a4b84730ee547ad09e
Nicolai Kozel <nicolaikozel@live.ca>
2021-03-08 17:03:34 -0500

    Fix centering

--- graphics/common.py
+++ graphics/common.py
@@ -2,7 +2,5 @@ def get_x_pos_for_centered_text(center_pos: int, text: str, font_width: int) -> 
     return abs(center_pos - ((len(text) * font_width) / 2))
 
 
-def get_x_pos_for_centered_text_and_obj(
-    center_pos: int, text: str, font_width: int, obj_width: int
-) -> int:
-    return abs(center_pos - ((obj_width + (len(text) * font_width)) / 2))
+def get_x_pos_for_centered_obj(center_pos: int, obj_width: int) -> int:
+    return abs(center_pos - (obj_width / 2))
--- views/weather.py
+++ views/weather.py
@@ -6,16 +6,19 @@ from rgbmatrix import FrameCanvas, graphics, RGBMatrix
 
 from constants import Color, Font
 from data import Data
-from graphics.common import get_x_pos_for_centered_text_and_obj
+from graphics.common import get_x_pos_for_centered_obj
 from utils import get_abs_file_path
 from views.base_views import BaseView
 from weather.data_classes import WeatherCondition
 from weather.open_weather import OpenWeatherDataThread
 
-ICON_WIDTH = 19
+CELCIUS_INDICATOR_WIDTH = 3
+CONDITION_ICON_TEMP_MARGIN = 3
+CONDITION_ICON_WIDTH = 15
 WEATHER_CONDITION_ICON_MAP = {
     WeatherCondition.CLEAR: dict(
         name="sunny",
+        x_offset=-1,
         y_offset=1,
     ),
     WeatherCondition.CLOUDS: dict(
@@ -47,9 +50,7 @@ class WeatherView(BaseView):
         super().__init__(rgb_matrix)
         self._offscreen_canvas = self._rgb_matrix.CreateFrameCanvas()
         self._icon = None
-        self._icon_x_offset = 0
-        self._icon_y_offset = 0
-        self._condition = None
+        self._icon_name = None
         self._temperature_color = temperature_color
 
     def _render_temperature(
@@ -71,47 +72,52 @@ class WeatherView(BaseView):
             self._temperature_color.value,
         )
 
-    def _render_condition(self, condition: WeatherCondition, x_pos: int):
-        if condition != self._condition:
-            icon_data = WEATHER_CONDITION_ICON_MAP[condition]
+    def _render_condition(self, icon_name: str, x_pos: int, y_pos: int):
+        if icon_name != self._icon_name:
             self._icon = Image.open(
-                get_abs_file_path(f"assets/{icon_data['name']}.ppm")
+                get_abs_file_path(f"assets/{icon_name}.ppm")
             ).convert("RGB")
-            self._icon_x_offset = icon_data.get("x_offset", 0)
-            self._icon_y_offset = icon_data.get("y_offset", 0)
-            self._condition = condition
+            self._icon_name = icon_name
         self._icon.resize(
             (self._rgb_matrix.width, self._rgb_matrix.height), Image.ANTIALIAS
         )
         self._offscreen_canvas.SetImage(
-            self._icon, x_pos + self._icon_x_offset, self._icon_y_offset, unsafe=False
+            self._icon, x_pos, y_pos, unsafe=False
         )
 
     def _render(self):
         self._offscreen_canvas.Clear()
-
+        # Get weather data
         weather_data = Data.get("weather")
-        temperature = "-10"
+        temperature = "0"
         condition = WeatherCondition.CLOUDS
         if weather_data:
-            # temperature = str(weather_data.current.temperature)
+            temperature = str(weather_data.current.temperature)
             condition = weather_data.current.condition
+    
+        # Get condition icon data and determine x_pos
+        condition_icon_data = WEATHER_CONDITION_ICON_MAP[condition]
         font, size = self._get_font(Font.TINY)
+        font_width = len(temperature)*size["width"]
+        margin_width = CONDITION_ICON_TEMP_MARGIN - 1 if temperature.startswith("-") else CONDITION_ICON_TEMP_MARGIN
         x_pos = (
-            get_x_pos_for_centered_text_and_obj(
+            get_x_pos_for_centered_obj(
                 center_pos=16,
-                text=temperature,
-                font_width=size["width"],
-                obj_width=ICON_WIDTH,
-            )
-            - 1
+                obj_width=CONDITION_ICON_WIDTH+margin_width+font_width+CELCIUS_INDICATOR_WIDTH,
+            ) + condition_icon_data.get("x_offset", 0)
         )
-        self._render_condition(condition=condition, x_pos=x_pos)
+
+        # Render condition icon and temperature
+        self._render_condition(
+            icon_name=condition_icon_data["name"],
+            x_pos=x_pos,
+            y_pos=condition_icon_data.get("y_offset", 0),
+        )
         self._render_temperature(
             temperature=temperature,
             font=font,
             font_size=size,
-            x_pos=x_pos + ICON_WIDTH - 1,
+            x_pos=x_pos + CONDITION_ICON_WIDTH + margin_width,
         )
 
         self._offscreen_canvas = self._rgb_matrix.SwapOnVSync(self._offscreen_canvas)
