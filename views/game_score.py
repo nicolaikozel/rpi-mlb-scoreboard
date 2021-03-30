2dcc2b20e416d4d13302ffe2be8bf84d19f895e0
Nicolai Kozel <nicolaikozel@live.ca>
2021-03-29 20:30:05 -0400

    Start fetching colors

--- views/game_score.py
+++ views/game_score.py
@@ -15,13 +15,18 @@ class GameScoreView(BaseView):
         super().__init__(rgb_matrix)
         self._font, self._font_size = Font.get_font(FontStyle.SMALL)
 
-    def _render_team_name_and_runs(self, team_name: str, runs: int, x_pos: int, y_pos: int):
+    def _render_score_for_team(self, team_name: str, runs: int, x_pos: int, y_pos: int):
+        team_colors = TEAM_COLORS[team_name]
+        text_color_rgb = team_colors.get("text", {"r": 255, "g": 255, "b": 255})
+        text_color = graphics.Color(text_color_rgb["r"], text_color_rgb["g"], text_color_rgb["b"])
+        accent_color_rgb = team_colors.get("accent", {"r": 0, "g": 0, "b": 0})
+        accent_color = graphics.Color(accent_color_rgb["r"], accent_color_rgb["g"], accent_color_rgb["b"])
         graphics.DrawText(
             self._offscreen_canvas,
             self._font,
             x_pos,
             y_pos,
-            Color.RED.value,
+            text_color,
             TEAM_ABBREVIATIONS[team_name],
         )
         graphics.DrawText(
@@ -29,20 +34,20 @@ class GameScoreView(BaseView):
             self._font,
             x_pos+20,
             y_pos,
-            Color.BLUE.value,
+            text_color,
             str(runs),
         )
 
     def _render(self):
         current_game = Data.get("current_game")
         if current_game:
-            self._render_team_name_and_runs(
+            self._render_score_for_team(
                 team_name=current_game.overview.home_team_name,
                 runs=current_game.overview.home_team_runs,
                 x_pos=1,
                 y_pos=7,
             )
-            self._render_team_name_and_runs(
+            self._render_score_for_team(
                 team_name=current_game.overview.away_team_name,
                 runs=current_game.overview.away_team_runs,
                 x_pos=1,
